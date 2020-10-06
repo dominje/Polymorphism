d3c852402c55060c4de0e6284b0ac8e126db82e7
dominje <71994674+dominje@users.noreply.github.com>
2020-10-06 18:30:43 +0800

    Adding answers for Polymorphism Exercise

---
+++ Polymorphism/src/Print.java
@@ -0,0 +1,15 @@
+public class Print {
+    public static void print() {
+        System.out.println("Print!");
+    }
+
+    public static void print(String red, String blue, String green){
+        System.out.println("This is a colored printer!");
+    }
+
+    public static void print(String black){
+        System.out.println("This is a black and white printer!");
+    }
+}
+
+
---
+++ Polymorphism/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args){
+        Print print = new Print();
+        print.print();
+        print.print("black");
+        print.print("red", "blue", "green");
+    }
+
+}
